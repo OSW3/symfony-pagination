2c7ebb4956a859f40ce79f8a12f5aa41a77f8cbc
Arnaud BODEL <arnaud.bodel@gmail.com>
2024-07-25 01:19:06 +0200

    -

--- src/PaginationBundle.php
+++ src/PaginationBundle.php
@@ -1,14 +1,14 @@
 <?php 
 namespace OSW3\Pagination;
 
-use OSW3\Pagination\Utils\ConfigurationYaml;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use OSW3\Pagination\DependencyInjection\Configuration;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class PaginationBundle extends Bundle
 {
     public function build(ContainerBuilder $container): void
     {
-        ConfigurationYaml::write($container->getParameter('kernel.project_dir'));
+        (new Configuration)->generateProjectConfig($container->getParameter('kernel.project_dir'));
     }
 }
